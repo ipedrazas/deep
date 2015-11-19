9416a4a50b687c8a6835bbc330f20124b5bd26cc
Ivan Pedrazas <ipedrazas@gmail.com>
2015-11-19 10:43:37 +0000

    activate testsuites

--- reports-api/api.py
+++ reports-api/api.py
@@ -2,19 +2,19 @@ from eve import Eve
 from flask.ext.cors import CORS
 import json
 from flask import request
-# from xml2json import xml_to_json
+from xml2json import xml_to_json
 
 
-# def before_insert(resource_name, items):
-#     for item in items:
-#         item['report'] = xml_to_json(item['report'])
-#         print str(item)
+def before_insert(resource_name, items):
+    for item in items:
+        item['report'] = xml_to_json(item['report'])
+        print str(item)
 
 
 app = Eve()
 cors = CORS(app)
 
-# app.on_insert += before_insert
+app.on_insert += before_insert
 
 
 @app.route("/version")
