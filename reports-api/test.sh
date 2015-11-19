e024bdfa2f3e5024cb287c066f17604f4c879916
Ivan Pedrazas <ipedrazas@gmail.com>
2015-11-19 12:52:47 +0000

    web submodule moved out

--- reports-api/test.sh
+++ reports-api/test.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 
-export HOST="localhost"
+export HOST="deep-reports.ipedrazas.k8s.co.uk";
 
+# export HOST="localhost";
+export BUILD_ID="$(date +%s)";
 http POST $HOST:5000/reports timestamp="$(date +%s)" branch="master" build="ts_$BUILD_ID" repo="ipedrazas/deep-reports" report="$(cat reports.xml)"
 
 
